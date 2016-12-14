43f5f3ab4e60435f1900130054592d1e68a2983a
Jacob <yacoubium@yahoo.de>
2016-12-14 20:55:36 +0100

    infra

---
+++ springBoot/src/main/java/com/jacob/jsmart/rest/springBoot/App.java
@@ -0,0 +1,11 @@
+package com.jacob.jsmart.rest.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
